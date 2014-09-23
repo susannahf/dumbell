89079c303435ffc4b002be7f6635a67959157f75
susannahf <susannah.fleming@phc.ox.ac.uk>
2014-09-23 11:49:38 +0100

    add code for input of LR

---
+++ dbplotLR.R
@@ -0,0 +1,35 @@
+# function for making dumbbell plots
+# to run the function, you first import it using source('dbplot.R')
+# you can then call dbplotLR() as a function
+# inputs are prevalence, and negative and positive likelihood ratios
+
+dbplotLR <- function(priors, negLR, posLR,lablmag=7,textsz=1){
+
+# post-test probabilities
+testnegs = 100 * (negLR * priors) / (1 - priors + (negLR * priors))
+testpos = 100 * (posLR * priors) / (1 - priors + (posLR * priors))
+
+# indices for y values
+idx = 1:length(priors)
+
+# extend left hand margin to allow for labels (2nd value is relevant one)
+par(mar=c(5,lablmag,4,2))
+
+# plot priors, set x and y axes
+plot(priors,idx,type="p",col="blue", pch=16, xlim=c(0, 100), xlab="Probability (%)", ylab="", yaxt="n", frame.plot=FALSE)
+# plot lines
+for(i in idx) lines(x=c(testnegs[i], testpos[i]), y=c(i, i), type="l", lty=2)
+# plot testpos and testneg
+points(x=testpos,y=idx,type="p",col="green", pch=16)
+points(x=testnegs,y=idx,type="p",col="red",pch=16)
+# plot priors again to overlay
+points(x=priors,y=idx,type="p",col="blue",pch=16)
+# add labels to y axis
+#axis(2, at=idx, labels=labls, las=1,cex.axis=textsz, tick=FALSE)
+return(1)
+}
+
+
+
+# lty = 1 solid, 2 dashed, 3 dotted, 4 dotdash, 5 longdash, 6 twodash
+# pch see http://www.statmethods.net/advgraphs/parameters.html
